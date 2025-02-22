ba1c6fc21c37ff3ee2d5bcb1fdeae1823eb9bdbf
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-02-22 13:49:46 +0000

    Add router, controller and view files for home page

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 require("dotenv").config();
 const express = require("express");
+const indexRouter = require("./routes/indexRouter");
 const app = express();
+const path = require("node:path")
 
+app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs");
+
+const assetsPath = path.join(__dirname, "public")
+app.use(express.static(assetsPath))
+
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/", indexRouter)
+// app.use("/games", gameRouter)
+// app.use("/developers", developerRouter)
+// app.use("/genres", genreRouter)
+
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Express app running at port ${PORT}`);
 });
---
+++ controllers/indexController.js
@@ -0,0 +1,5 @@
+function getHomePage(req, res) {
+  res.render("pages/index");
+}
+
+module.exports = { getHomePage };
