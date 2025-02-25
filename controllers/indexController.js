e19a7a218a4d3785ac5d6361eaf220eb0ea95f37
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-02-25 11:39:58 +0000

    Update home page to render accurate entity counts

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,10 @@
-function getHomePage(req, res) {
-  res.render("pages/index");
+const db = require("../db/queries");
+
+async function getHomePage(req, res) {
+  const gameCount =  await db.getGamesCount();
+  const developerCount = await db.getDevelopersCount();
+  const genreCount = await db.getGenresCount();
+  res.render("pages/index", { gameCount, developerCount, genreCount });
 }
 
 module.exports = { getHomePage };
